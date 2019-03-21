1827562ae10efc09bc8e4c38b1770cc9a28d4a10
Ryan Van Slooten <ryanvs@yahoo.com>
2019-03-21 15:24:13 -0500

    Initial code

---
+++ SerialPortTester/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SerialPortTester
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                    components.Dispose();
+                if (_port != null)
+                    _port.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReceivedTextBox = new System.Windows.Forms.TextBox();
+            this.SendTextBox = new System.Windows.Forms.TextBox();
+            this.ConfigButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ReceivedTextBox
+            // 
+            this.ReceivedTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReceivedTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ReceivedTextBox.Multiline = true;
+            this.ReceivedTextBox.Name = "ReceivedTextBox";
+            this.ReceivedTextBox.ReadOnly = true;
+            this.ReceivedTextBox.Size = new System.Drawing.Size(575, 100);
+            this.ReceivedTextBox.TabIndex = 0;
+            // 
+            // SendTextBox
+            // 
+            this.SendTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendTextBox.Location = new System.Drawing.Point(12, 118);
+            this.SendTextBox.Multiline = true;
+            this.SendTextBox.Name = "SendTextBox";
+            this.SendTextBox.Size = new System.Drawing.Size(575, 100);
+            this.SendTextBox.TabIndex = 0;
+            // 
+            // ConfigButton
+            // 
+            this.ConfigButton.Location = new System.Drawing.Point(12, 232);
+            this.ConfigButton.Name = "ConfigButton";
+            this.ConfigButton.Size = new System.Drawing.Size(92, 28);
+            this.ConfigButton.TabIndex = 1;
+            this.ConfigButton.Text = "Configure";
+            this.ConfigButton.UseVisualStyleBackColor = true;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(110, 232);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(92, 28);
+            this.OpenButton.TabIndex = 1;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(208, 232);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(92, 28);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(306, 232);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(92, 28);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 267);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.ConfigButton);
+            this.Controls.Add(this.SendTextBox);
+            this.Controls.Add(this.ReceivedTextBox);
+            this.Name = "MainForm";
+            this.Text = "Serial Port Tester";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ReceivedTextBox;
+        private System.Windows.Forms.TextBox SendTextBox;
+        private System.Windows.Forms.Button ConfigButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button SendButton;
+    }
+}
+
---
+++ SerialPortTester/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortTester
+{
+    public partial class MainForm : Form
+    {
+        private SerialPort _port;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _port = new SerialPort();
+            _port.DataReceived += _port_DataReceived;
+            _port.ErrorReceived += _port_ErrorReceived;
+            _port.PinChanged += _port_PinChanged;
+
+            var names = SerialPort.GetPortNames();
+            Trace.TraceInformation("PortNames: {0}", string.Join(", ", names));
+            _port.PortName = "COM1";
+            _port.BaudRate = 9600;
+            _port.DataBits = 8;
+            _port.Handshake = Handshake.None;
+            _port.Parity = Parity.None;
+            _port.StopBits = StopBits.One;
+            _port.DtrEnable = false;
+            _port.RtsEnable = false;
+            _port.ReadTimeout = 1000;
+            _port.WriteTimeout = 250;
+
+            UpdateButtons();
+        }
+
+        private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Trace.TraceInformation("DataReceived: {0}", e.EventType);
+            string data = ((SerialPort)sender).ReadExisting();
+            ReceivedTextBox.Text += data;
+        }
+
+        private void _port_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            Trace.TraceInformation("ErrorReceived: {0}", e.EventType);
+        }
+
+        private void _port_PinChanged(object sender, SerialPinChangedEventArgs e)
+        {
+            Trace.TraceInformation("PinChanged: {0}", e.EventType);
+        }
+
+        private void UpdateButtons()
+        {
+            bool isOpen = _port.IsOpen;
+            ConfigButton.Enabled = !isOpen;
+            OpenButton.Enabled = !isOpen;
+            CloseButton.Enabled = isOpen;
+            SendButton.Enabled = isOpen;
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            if (!_port.IsOpen)
+                _port.Open();
+            UpdateButtons();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            if (_port.IsOpen)
+                _port.Close();
+            UpdateButtons();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            _port.Write(SendTextBox.Text);
+            UpdateButtons();
+        }
+    }
+}
